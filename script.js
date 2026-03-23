f41c6bf55673fe79011269de3ca955cd7969c924
Nishanth <nishanthnithu36@gmail.com>
2026-03-24 00:43:56 +0530

    first

---
+++ script.js
@@ -0,0 +1,5 @@
+const box = document.getElementById("mybox")
+box.addEventListener("click",event=>{
+    event.target.style.backgroundColor = "gray"
+    event.target.textContent="You are a 🐃🤣🤣"
+})
