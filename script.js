f26dd73f87531886fde5ef5bd0bfe9f1b2da0068
Nishanth <nishanthnithu36@gmail.com>
2026-03-24 00:52:38 +0530

    first

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const box = document.getElementById("mybox")
 box.addEventListener("click",event=>{
     event.target.style.backgroundColor = "gray"
-    event.target.textContent="You are a 🐃🤣🤣"
+    event.target.textContent="You are Earuma 🐃🤣🤣"
+})
+box.addEventListener("mouseover",event=>{
+    event.target.style.backgroundColor = "red"
+    event.target.textContent="vara vala ellaya  ok click pannu"
+})
+box.addEventListener("mouseout",event=>{
+    event.target.style.backgroundColor = " rgb(0, 191, 255)"
+    event.target.textContent="bye bye I have work "
 })
