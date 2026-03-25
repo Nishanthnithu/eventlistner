b90311d7c66afec62d00641dc589be63654b169a
Nishanth <nishanthnithu36@gmail.com>
2026-03-25 21:52:43 +0530

    4

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const box = document.getElementById("mybox")
+const movement=10
+let x=0
+let y=0
 box.addEventListener("click",event=>{
     event.target.style.backgroundColor = "gray"
     event.target.textContent="Have a  nie day 🤣🤣"
@@ -19,4 +22,27 @@ document.addEventListener("keyup",event=>{
 document.addEventListener("keydown",event=>{
     box.textContent= "🥳"
     
+})
+
+document.addEventListener("keydown",event=>{
+    if(event.key.startsWith("Arrow")){
+        switch (event.key) {
+            case "ArrowUp":
+                y-=movement 
+                break;
+            case "ArrowDown":
+                y +=movement 
+                break;
+                case "ArrowLeft":
+                x-=movement 
+                break;
+                case "ArrowRight":
+                x+=movement 
+                break;
+            default:
+                break;
+        }
+        box.style.top=`${y}px`
+        box.style.left=`${x}px`
+    }
 })
